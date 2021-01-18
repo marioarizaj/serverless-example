57f1a8e524bb70e5b392144e305ef89e2ee047d8
mario <marizaj15@hotmail.com>
2021-01-18 10:17:39 +0100

    Add serverless policy

--- api/create_author/main.go
+++ api/create_author/main.go
@@ -1,12 +1,7 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-
 	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/marioarizaj/serverless-example/models"
 )
 
 // Response is of type APIGatewayProxyResponse since we're leveraging the
@@ -15,28 +10,28 @@ import (
 // https://serverless.com/framework/docs/providers/aws/events/apigateway/#lambda-proxy-integration
 type Response events.APIGatewayProxyResponse
 
-// Handler is our lambda handler invoked by the `lambda.Start` function call
-func Handler(request events.APIGatewayProxyRequest) (Response, error) {
-	var newAuthor models.Author
-	err := json.Unmarshal([]byte(request.Body), &newAuthor)
-	if err != nil {
-		return Response{StatusCode: 404}, err
-	}
-	json.HTMLEscape(&buf, body)
-
-	resp := Response{
-		StatusCode:      200,
-		IsBase64Encoded: false,
-		Body:            buf.String(),
-		Headers: map[string]string{
-			"Content-Type":           "application/json",
-			"X-MyCompany-Func-Reply": "hello-handler",
-		},
-	}
-
-	return resp, nil
-}
-
-func main() {
-	lambda.Start(Handler)
-}
+// // Handler is our lambda handler invoked by the `lambda.Start` function call
+// func Handler(request events.APIGatewayProxyRequest) (Response, error) {
+// 	var newAuthor models.Author
+// 	err := json.Unmarshal([]byte(request.Body), &newAuthor)
+// 	if err != nil {
+// 		return Response{StatusCode: 404}, err
+// 	}
+// 	json.HTMLEscape(&buf, body)
+//
+// 	resp := Response{
+// 		StatusCode:      200,
+// 		IsBase64Encoded: false,
+// 		Body:            buf.String(),
+// 		Headers: map[string]string{
+// 			"Content-Type":           "application/json",
+// 			"X-MyCompany-Func-Reply": "hello-handler",
+// 		},
+// 	}
+//
+// 	return resp, nil
+// }
+//
+// func main() {
+// 	lambda.Start(Handler)
+// }
